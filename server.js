2c4c3c35bfb1d4bd32702451c837ee32ae52f102
José Pedro Schardosim Simão <josepedrosimao@gmail.com>
2022-07-19 19:42:55 -0300

    Add CORS middleware

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const userRoutes = require("./routes/user.routes");
 
 const bodyParser = require("body-parser");
 const app = express();
+var cors = require('cors')
 const port = process.env.PORT;
 
 require("./config/database").connect();
 
+app.use(cors())
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
